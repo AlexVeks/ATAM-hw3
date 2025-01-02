18471831b3d4e58336149ca0acae89e5ff43e2dd
giladby4 <glad.ben@campus.technion.ac.il>
2025-01-02 10:40:10 +0000

    part2

--- part2/.ili.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/student/Desktop/hw3/ATAM-hw3/part2/ili.o := ld -m elf_x86_64 -z max-page-size=0x200000   -r -o /home/student/Desktop/hw3/ATAM-hw3/part2/ili.o /home/student/Desktop/hw3/ATAM-hw3/part2/ili_main.o /home/student/Desktop/hw3/ATAM-hw3/part2/ili_utils.o /home/student/Desktop/hw3/ATAM-hw3/part2/ili_handler.o 
